423a972d833939ce63fedd1338ce54511a612217
siria55 <siria55lee@gmail.com>
2022-08-31 18:03:46 +0800

    update

--- 0452_Minimum_Number_of_Arrows_to_Burst_Balloons/Solution1.cpp
+++ 0452_Minimum_Number_of_Arrows_to_Burst_Balloons/Solution1.cpp
@@ -1,4 +1,5 @@
-#include <iostream>
+#include <cstdio>
+#include <string>
 #include <vector>
 #include <algorithm>
 using namespace std;
@@ -6,49 +7,46 @@ using namespace std;
 class Solution {
 public:
     int findMinArrowShots(vector<vector<int>>& points) {
-        sort(points.begin(), points.end(),
-            [](vector<int>& a, vector<int>& b) {
-                return a[1] < b[1];
-            }
-        );
-        int arrow = 1;
+        sort(points.begin(), points.end(), [](vector<int> &a, vector<int> &b) {
+            return a[1] < b[1];
+        });
+        int res = 1;
+        int size = points.size();
         int tail = points[0][1];
-        int N = points.size();
-        for (int i = 1; i < N; i++) {
+        for (int i = 1; i < size; ++i) {
             if (tail < points[i][0]) {
-                arrow++;
+                ++res;
                 tail = points[i][1];
             }
         }
-        return arrow;
+        return res;
     }
 };
-
 
 void test(string test_name, vector<vector<int>>& points, int expected)
 {
     int res = Solution().findMinArrowShots(points);
     if (res == expected)
-        cout << test_name << " succeed" << endl;
+        printf("%s succeed\n", test_name.c_str());
     else
-        cout << test_name << " fail" << endl;
+        printf("%s fail\n", test_name.c_str());
 }
 
 int main()
 {
-    vector<vector<int>> points1 = {
+    vector<vector<int>> points1{
         {10, 16}, {2, 8}, {1, 6}, {7, 12}
     };
     int expected1 = 2;
     test("test1", points1, expected1);
 
-    vector<vector<int>> points2 = {
+    vector<vector<int>> points2{
         {1,2}, {3,4}, {5,6}, {7,8}
     };
     int expected2 = 4;
     test("test2", points2, expected2);
 
-    vector<vector<int>> points3 = {
+    vector<vector<int>> points3{
         {1,2}, {2,3}, {3,4}, {4,5}
     };
     int expected3 = 2;
--- t.py
+++ t.py
@@ -1,41 +1,56 @@
-from typing import *
+from typing import List
+
 
 class Solution:
-    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        size = len(flowerbed)
-        i = 0
-        while i < size:
-            if flowerbed[i]:
-                i += 2
-            else:
-                if i == size - 1 or flowerbed[i+1] == 0:
-                    i += 2
-                    n -= 1
-                else:
-                    i += 3
-        return n <= 0
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        points.sort(key=lambda x:x[1])
+        size = len(points)
+        res = 1
+        tail = points[0][1]
+        for i in range(1, size):
+            if tail < points[i][0]:
+                res += 1
+                tail = points[i][1]
+        return res
 
 
-def test(test_name, flowerbed, n, expected):
-    res = Solution().canPlaceFlowers(flowerbed, n)
+def test(test_name, points, expected):
+    res = Solution().findMinArrowShots(points)
     if res == expected:
         print(test_name + ' succeed')
     else:
         print(test_name + ' fail')
 
 
-if __name__ == '__main__':
-    flowerbed1 = [1,0,0,0,1]
-    n1 = 1
-    expected1 = True
-    test('test1', flowerbed1, n1, expected1)
+if __name__ == "__main__":
+    points1 = [[10,16],[2,8],[1,6],[7,12]]
+    expected1 = 2
+    test('test1', points1, expected1)
 
-    flowerbed2 = [1,0,0,0,1]
-    n2 = 2
-    expected2 = False
-    test('test2', flowerbed2, n2, expected2)
+    points2 = [[1,2],[3,4],[5,6],[7,8]]
+    expected2 = 4
+    test('test2', points2, expected2)
 
-    flowerbed3 = [1,0,0,0,1,0,0]
-    n3 = 2
-    expected3 = True
-    test('test3', flowerbed3, n3, expected3)
+    points3 = [[1,2],[2,3],[3,4],[4,5]]
+    expected3 = 2
+    test('test3', points3, expected3)
+
+    points4 = [[1,2]]
+    expected4 = 1
+    test('test4', points4, expected4)
+
+    points5 = [[2,3],[2,3]]
+    expected5 = 1
+    test('test5', points5, expected5)
+
+    points6 = [[1,2],[2,3],[3,4],[4,5]]
+    expected6 = 2
+    test('test6', points6, expected6)
+
+    points7 = [[-2147483648,2147483647]]
+    expected7 = 1
+    test('test7', points7, expected7)
+
+    points8 = [[3,9],[7,12],[3,8],[6,8],[9,10],[2,9],[0,9],[3,9],[0,6],[2,8]]
+    expected8 = 2
+    test('test8', points8, expected8)
