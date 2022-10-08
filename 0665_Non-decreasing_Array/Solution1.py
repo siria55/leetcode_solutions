d17b7ba28d5a968f87d38819cb49e1f5af5ff3d3
siria55 <siria55lee@gmail.com>
2022-10-08 18:03:54 +0800

    update

--- 0665_Non-decreasing_Array/Solution1.py
+++ 0665_Non-decreasing_Array/Solution1.py
@@ -1,35 +1,32 @@
 from typing import *
 
+
 class Solution:
     def checkPossibility(self, nums: List[int]) -> bool:
-        cnt = 0
+        size = len(nums)
+        if size <= 1:
+            return True
 
-        for i in range(1, len(nums)):
-            # 需要单调递增（不用严格）<=是满足条件的，>是不满足的
-            if nums[i-1] > nums[i]:
-                cnt += 1
-
-                # 下面即是把不满足的两个数改为相等，具体改那个数有三种情况
-                # i-1 = 0，则改第一个数，为了i不影响后面的
-                if i == 1:
-                    nums[i-1] = nums[i]
-                # 尽量让 i-2 <= i-1 < i。这时能改i-1，就不要改i
-                elif i == 1 or nums[i] >= nums[i-2]:
-                    nums[i-1] = nums[i]
-                # 这时 i < i-2, i < i-1，    3 4 1 -> 3 4 4
-                # 改i-1也没用，改i
-                else:
-                    nums[i] = nums[i-1]
-
-        return cnt <= 1
+        moded = nums[0] > nums[1]
+        for i in range(1, size-1):
+            if nums[i] <= nums[i+1]:
+                continue
+            if moded:
+                return False
+            if nums[i-1] <= nums[i+1]:
+                nums[i] = nums[i+1]
+            else:
+                nums[i+1] = nums[i]
+            moded = True
+        return True
 
 
 def test(test_name, nums, expected):
     res = Solution().checkPossibility(nums)
     if res == expected:
-        print(test_name + ' success.')
+        print(test_name + ' succeed')
     else:
-        print(test_name + ' failed.')
+        print(test_name + ' fail')
 
 
 if __name__ == '__main__':
@@ -41,3 +38,6 @@ if __name__ == '__main__':
     expected2 = False
     test('test2', nums2, expected2)
 
+    nums3 = [3,4,2,3]
+    expected3 = False
+    test('test3', nums3, expected3)
