f2f3de1d3e86f6b6e428b79e94330d7989e9740f
siria55 <siria55lee@gmail.com>
2022-10-20 14:22:19 +0800

    update

--- t.cpp
+++ t.cpp
@@ -1,53 +1,38 @@
-#include <cstdio>
-#include <string>
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
-    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        int size = flowerbed.size();
-        for (int i = 0; i < size;) {
-            if (flowerbed[i]) {
-                i += 2;
-            } else {
-                if (i == size - 1 || flowerbed[i+1] == 0) {
-                    --n;
-                    i += 2;
-                } else {
-                    i += 3;
-                }
-            }
-        }
-        return n <= 0;
+    int findMinArrowShots(vector<vector<int>>& points) {
 
     }
 };
 
-void test(string test_name, vector<int>& flowerbed, int n, bool expected) {
-    bool res = Solution().canPlaceFlowers(flowerbed, n);
-    if (res == expected) {
+void test(string test_name, vector<vector<int>>& points, int expected)
+{
+    int res = Solution().findMinArrowShots(points);
+    if (res == expected)
         printf("%s succeed\n", test_name.c_str());
-    } else {
+    else
         printf("%s fail\n", test_name.c_str());
-    }
 }
 
-int main() {
-    vector<int> flowerbed1{1,0,0,0,1};
-    int n1 = 1;
-    bool expected1 = true;
-    test("test1", flowerbed1, n1, expected1);
+int main()
+{
+    vector<vector<int>> points1{
+        {10, 16}, {2, 8}, {1, 6}, {7, 12}
+    };
+    int expected1 = 2;
+    test("test1", points1, expected1);
 
-    vector<int> flowerbed2{1,0,0,0,1};
-    int n2 = 2;
-    bool expected2 = false;
-    test("test2", flowerbed2, n2, expected2);
+    vector<vector<int>> points2{
+        {1,2}, {3,4}, {5,6}, {7,8}
+    };
+    int expected2 = 4;
+    test("test2", points2, expected2);
 
-    vector<int> flowerbed3{1,0,0,0,1,0,0};
-    int n3 = 2;
-    bool expected3 = true;
-    test("test3", flowerbed3, n3, expected3);
+    vector<vector<int>> points3{
+        {1,2}, {2,3}, {3,4}, {4,5}
+    };
+    int expected3 = 2;
+    test("test3", points3, expected3);
 
     return 0;
 }
