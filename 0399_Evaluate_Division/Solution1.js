19f76abd42e0a9ea362f9205d3196e7bba91639b
siria55 <siria55lee@gmail.com>
2022-10-21 12:10:26 +0800

    update

--- 0399_Evaluate_Division/Solution1.js
+++ 0399_Evaluate_Division/Solution1.js
@@ -78,27 +78,3 @@ let values2 = [4.0, 3.0];
 let queries2 = [["a","b"],["e","e"],["x","x"]];
 let expected2 = [4/3, 1.0, -1.0];
 test('test2', equations2, values2, queries2, expected2);
-
-
-//  Equations are given in the format A / B = k, where A and B are variables 
-//  represented as strings, and k is a real number (floating point number). 
-//  Given some queries, return the answers. If the answer does not exist, return -1.0.
-
-//  Example:
-//  Given a / b = 2.0, b / c = 3.0.
-//  queries are: a / c = ?, b / a = ?, a / e = ?, a / a = ?, x / x = ? .
-//  return [6.0, 0.5, -1.0, 1.0, -1.0 ].
-
-//  The input is: vector<pair<string, string>> equations, vector<double>& values,
-//   vector<pair<string, string>> queries , where equations.size() == values.size(), 
-//   and the values are positive. This represents the equations. Return vector<double>.
-
-//  According to the example above:
-
-//  equations = [ ["a", "b"], ["b", "c"] ],
-//  values = [2.0, 3.0],
-//  queries = [ ["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"] ]. 
-//   
-
-//  The input is always valid. You may assume that evaluating the queries will
-//   result in no division by zero and there is no contradiction.
