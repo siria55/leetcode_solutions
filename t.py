eac43ca760ccbd75f4e28a541447b5cc69cde3bd
Siria <siria55lee@gmail.com>
2022-10-24 18:46:24 +0800

    update

--- t.py
+++ t.py
@@ -1,35 +1,32 @@
 from typing import *
 
+
 class Solution:
-    def partitionLabels(self, s: str) -> List[int]:
-        lasts = [0] * 26
-        res = []
-
-        for i, ch in enumerate(s):
-            lasts[ord(ch)-ord('a')] = i
-
-        start, end = 0, 0
-        for i, ch in enumerate(s):
-            end = max(end, lasts[ord(s[i])-ord('a')])
-            if end == i:
-                res.append(end + 1 - start)
-                start = end + 1
+    def maxProfit(self, prices: List[int]) -> int:
+        res = 0
+        size = len(prices)
+        for i in range(1, size):
+            res += max(prices[i] - prices[i-1], 0)
         return res
 
 
-def test(test_name, s, expected):
-    res = Solution().partitionLabels(s)
+def test(test_name, prices, expected):
+    res = Solution().maxProfit(prices)
     if res == expected:
         print(test_name + ' succeed')
     else:
         print(test_name + ' fail')
 
 
-if __name__ == '__main__':
-    s1 = 'ababcbacadefegdehijhklij'
-    expected1 = [9,7,8]
-    test('test1', s1, expected1)
+if __name__ == "__main__":
+    prices1 = [7,1,5,3,6,4]
+    expected1 = 7
+    test('test1', prices1, expected1)
 
-    s2 = 'eccbbbbdec'
-    expected2 = [10]
-    test('test2', s2, expected2)
+    prices2 = [1,2,3,4,5]
+    expected2 = 4
+    test('test2', prices2, expected2)
+
+    prices3 = [7,6,4,3,1]
+    expected3 = 0
+    test('test3', prices3, expected3)
