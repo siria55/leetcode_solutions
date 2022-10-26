49b9d52488ee052840a65e29f9f7073518b7c0d2
Siria <siria55lee@gmail.com>
2022-10-26 18:10:14 +0800

    update

--- 0406_Queue_Reconstruction_by_Height/Solution1.cpp
+++ 0406_Queue_Reconstruction_by_Height/Solution1.cpp
@@ -4,16 +4,15 @@
 #include <algorithm>
 using namespace std;
 
+
 class Solution {
 public:
     vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
-        sort(people.begin(), people.end(), 
-            [&](vector<int> &a, vector<int> &b) {
-                if (a[0] == b[0])
-                    return a[1] < b[1];
-                return a[0] > b[0];
-            }
-        );
+        sort(people.begin(), people.end(), [](vector<int> &a, vector<int> &b) {
+            if (a[0] == b[0])
+                return a[1] < b[1];
+            return a[0] > b[0];
+        });
         vector<vector<int>> res;
         int size = people.size();
         for (int i = 0; i < size; ++i) {
--- t.cpp
+++ t.cpp
@@ -1,43 +1,53 @@
 #include <cstdio>
 #include <string>
 #include <vector>
+#include <algorithm>
 using namespace std;
+
 
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int res = 0;
-        int size = prices.size();
-        for (int i = 1; i < size; ++i) {
-            res += max(prices[i] - prices[i-1], 0);
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        sort(people.begin(), people.end(), [](vector<int> &a, vector<int> &b) {
+            if (a[0] == b[0])
+                return a[1] < b[1];
+            return a[0] > b[0];
+        });
+        vector<vector<int>> res;
+        int size = people.size();
+        for (int i = 0; i < size; ++i) {
+            res.insert(res.begin() + people[i][1], people[i]);
         }
         return res;
     }
 };
 
-void test(string test_name, vector<int>& prices, int expected)
+void test(string test_name, vector<vector<int>>& people, vector<vector<int>>& expected)
 {
-    int res = Solution().maxProfit(prices);
-    if (res == expected) {
+    vector<vector<int>> res = Solution().reconstructQueue(people);
+    if (res == expected)
         printf("%s succeed\n", test_name.c_str());
-    } else {
+    else
         printf("%s fail\n", test_name.c_str());
-    }
 }
 
 int main()
 {
-    vector<int> prices1{7,1,5,3,6,4};
-    int expected1 = 7;
-    test("test1", prices1, expected1);
+    vector<vector<int>> people1{
+        {7,0}, {4,4}, {7,1}, {5,0}, {6,1}, {5,2}
+    };
+    vector<vector<int>> expected1{
+        {5,0}, {7,0}, {5,2}, {6,1}, {4,4}, {7,1}
+    };
+    test("test1", people1, expected1);
 
-    vector<int> prices2{1,2,3,4,5};
-    int expected2 = 4;
-    test("test2", prices2, expected2);
-
-    vector<int> prices3{7,6,4,3,1};
-    int expected3 = 0;
-    test("test3", prices3, expected3);
+    vector<vector<int>> people2{
+        {6,0},{5,0},{4,0},{3,2},{2,2},{1,4}
+    };
+    vector<vector<int>> expected2{
+        {4,0},{5,0},{2,2},{3,2},{1,4},{6,0}
+    };
+    test("test2", people2, expected2);
 
     return 0;
 }
