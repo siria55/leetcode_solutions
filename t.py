67ee0aefa5a2fbbf1de59eac0ca6c300a91610e8
Siria <siria55lee@gmail.com>
2022-10-26 18:46:05 +0800

    update

--- t.py
+++ t.py
@@ -1,32 +1,28 @@
 from typing import *
-
+import functools
 
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        res = 0
-        size = len(prices)
-        for i in range(1, size):
-            res += max(prices[i] - prices[i-1], 0)
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        def _comp(a, b):
+            if a[0] == b[0]:
+                return a[1] - b[1]
+            return b[0] - a[0]
+        people.sort(key=functools.cmp_to_key(_comp))
+        res = []
+        for pair in people:
+            res.insert(pair[1], pair)
         return res
 
 
-def test(test_name, prices, expected):
-    res = Solution().maxProfit(prices)
+def test(test_name, people, expected):
+    res = Solution().reconstructQueue(people)
     if res == expected:
         print(test_name + ' succeed')
     else:
         print(test_name + ' fail')
 
 
-if __name__ == "__main__":
-    prices1 = [7,1,5,3,6,4]
-    expected1 = 7
-    test('test1', prices1, expected1)
-
-    prices2 = [1,2,3,4,5]
-    expected2 = 4
-    test('test2', prices2, expected2)
-
-    prices3 = [7,6,4,3,1]
-    expected3 = 0
-    test('test3', prices3, expected3)
+if __name__ == '__main__':
+    people1 = [[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+    expected1 = [[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
+    test('test1', people1, expected1)
