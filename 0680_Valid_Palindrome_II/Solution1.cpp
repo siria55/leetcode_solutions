4f7c54ba0458463c962dd65e772d9d32d8d83858
Siria <siria55lee@gmail.com>
2022-11-03 16:36:12 +0800

    update

--- 0680_Valid_Palindrome_II/Solution1.cpp
+++ 0680_Valid_Palindrome_II/Solution1.cpp
@@ -1,12 +1,12 @@
-#include <iostream>
+#include <cstdio>
+#include <string>
 using namespace std;
 
-
 class Solution {
-    bool isPal(const string& s, int l, int r) {
-        while (l < r) {
-            if (s[l++] != s[r--]) return false;
-        }
+    bool isPal(const string &s, int l, int r) {
+        while (l < r)
+            if (s[l++] != s[r--])
+                return false;
         return true;
     }
 public:
@@ -14,23 +14,20 @@ public:
         int l = 0, r = s.size() - 1;
         while (l < r) {
             if (s[l] != s[r])
-                return isPal(s, l+1, r) ||
-                       isPal(s, l, r-1);
-            l++, r--;
+                return isPal(s, l+1, r) || isPal(s, l, r-1);
+            ++l;
+            --r;
         }
         return true;
     }
 };
 
-
-
 void test(string test_name, string s, bool expected) {
     bool res = Solution().validPalindrome(s);
-    if (res == expected) {
-        cout << test_name + " succeed" << endl;
-    } else {
-        cout << test_name + " fail" << endl;
-    }
+    if (res == expected)
+        printf("%s succeed\n", test_name.c_str());
+    else
+        printf("%s fail\n", test_name.c_str());
 }
 
 int main() {
--- t.cpp
+++ t.cpp
@@ -1,24 +1,29 @@
 #include <cstdio>
 #include <string>
-#include <cmath>
 using namespace std;
 
 class Solution {
+    bool isPal(const string &s, int l, int r) {
+        while (l < r)
+            if (s[l++] != s[r--])
+                return false;
+        return true;
+    }
 public:
-    bool judgeSquareSum(int c) {
-        long l = 0, r = sqrt(c);
-        while (l <= r) {
-            long s = l * l + r * r;
-            if (s < c) ++l;
-            else if (s > c) --r;
-            else return true;
+    bool validPalindrome(string s) {
+        int l = 0, r = s.size() - 1;
+        while (l < r) {
+            if (s[l] != s[r])
+                return isPal(s, l+1, r) || isPal(s, l, r-1);
+            ++l;
+            --r;
         }
-        return false;
+        return true;
     }
 };
 
-void test(string test_name, int c, bool expected) {
-    bool res = Solution().judgeSquareSum(c);
+void test(string test_name, string s, bool expected) {
+    bool res = Solution().validPalindrome(s);
     if (res == expected)
         printf("%s succeed\n", test_name.c_str());
     else
@@ -26,29 +31,17 @@ void test(string test_name, int c, bool expected) {
 }
 
 int main() {
-    int c1 = 5;
+    string s1 = "aba";
     bool expected1 = true;
-    test("test1", c1, expected1);
+    test("test1", s1, expected1);
 
-    int c2 = 3;
-    bool expected2 = false;
-    test("test2", c2, expected2);
+    string s2 = "abca";
+    bool expected2 = true;
+    test("test2", s2, expected2);
 
-    int c3 = 4;
-    bool expected3 = true;
-    test("test3", c3, expected3);
-
-    int c4 = 2;
-    bool expected4 = true;
-    test("test4", c4, expected4);
-
-    int c5 = 1;
-    bool expected5 = true;
-    test("test5", c5, expected5);
-
-    int c6 = 2147482647;
-    bool expected6 = false;
-    test("test6", c6, expected6);
+    string s3 = "abc";
+    bool expected3 = false;
+    test("test3", s3, expected3);
 
     return 0;
 }
