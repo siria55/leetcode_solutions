21b399e8e51050c277024c2a38e958e2ab4b96d9
Siria <siria55lee@gmail.com>
2022-11-14 18:16:07 +0800

    update

--- 0633_Sum_of_Square_Numbers/Solution1.js
+++ 0633_Sum_of_Square_Numbers/Solution1.js
@@ -2,33 +2,36 @@
  * @param {number} c
  * @return {boolean}
  */
-var judgeSquareSum = function(c) {
-    let l = 0, r = Math.floor(Math.sqrt(c));
-    let sum;
-    while (l <= r) {
-      sum = l * l + r * r;
-      if (sum < c)
-        ++l;
-      else if (sum > c)
-        --r;
-      else
-        return true;
-    }
-    return false;
+ var judgeSquareSum = function(c) {
+  let l = 0, r = Math.floor(Math.sqrt(c));
+  while (l <= r) {
+    const s = l * l + r * r;
+    if (s > c)
+      r--;
+    else if (s < c)
+      l++;
+    else
+      return true;
+  }
+  return false;
 };
 
 function test(test_name, c, expected) {
-  let res = judgeSquareSum(c);
-  if (res == expected)
-    console.log(test_name + ' succeed');
-  else
-    console.log(test_name + ' fail');
+let res = judgeSquareSum(c);
+if (res == expected)
+  console.log(test_name + " succeed");
+else
+  console.log(test_name + " fail");
 }
 
-let c1 = 5;
-let expected1 = true;
-test('test1', c1, expected1);
+const c1 = 5;
+const expected1 = true;
+test("test1", c1, expected1);
 
-let c2 = 3;
-let expected2 = false;
-test('test2', c2, expected2)
+const c2 = 3;
+const expected2 = false;
+test("test2", c2, expected2);
+
+const c3 = 2;
+const expected3 = true;
+test("test3", c3, expected3);
--- t.js
+++ t.js
@@ -1,65 +1,37 @@
 /**
- * @param {string} s
- * @param {string} t
- * @return {string}
+ * @param {number} c
+ * @return {boolean}
  */
-var minWindow = function(s, t) {
-  if (s.length < t.length)
-    return "";
-
-  let counter = Array(128).fill(0);
-  let flag = Array(128).fill(false);
-  for (const ch of t) {
-    counter[ch.charCodeAt(0)]++;
-    flag[ch.charCodeAt(0)] = true;
-  }
-
-  let l = 0;
-  let cnt = 0, start = 0, minLength = s.length + 1;
-  for (let r = 0; r < s.length; r++) {
-    if (!flag[s.charCodeAt(r)])
-      continue;
-    if (--counter[s.charCodeAt(r)] >= 0)
-      cnt++;
-    while (cnt === t.length) {
-      if (minLength > r + 1 - l) {
-        minLength = r + 1 - l;
-        start = l;
-      }
-      if (flag[s.charCodeAt(l)] && ++counter[s.charCodeAt(l)] > 0)
-        cnt--;
-      l++;
+var judgeSquareSum = function(c) {
+    let l = 0, r = Math.floor(Math.sqrt(c));
+    while (l <= r) {
+      const s = l * l + r * r;
+      if (s > c)
+        r--;
+      else if (s < c)
+        l++;
+      else
+        return true;
     }
-  }
-  return minLength === s.length + 1
-    ? ""
-    : s.substring(start, start + minLength);
+    return false;
 };
 
-function test(test_name, s, t, expected) {
-  let res = minWindow(s, t);
-  if (res === expected)
-    console.log(test_name + ' succeed');
+function test(test_name, c, expected) {
+  let res = judgeSquareSum(c);
+  if (res == expected)
+    console.log(test_name + " succeed");
   else
-    console.log(test_name + ' fail');
+    console.log(test_name + " fail");
 }
 
-let s1 = 'ADOBECODEBANC';
-let t1 = 'ABC';
-let expected1 = 'BANC';
-test('test1', s1, t1, expected1);
+const c1 = 5;
+const expected1 = true;
+test("test1", c1, expected1);
 
-let s2 = 'a';
-let t2 = 'a';
-let expected2 = 'a';
-test('test2', s2, t2, expected2);
+const c2 = 3;
+const expected2 = false;
+test("test2", c2, expected2);
 
-let s3 = 'a';
-let t3 = 'aa';
-let expected3 = '';
-test('test3', s3, t3, expected3);
-
-let s4 = 'a';
-let t4 = 'b';
-let expected4 = '';
-test('test4', s4, t4, expected4);
+const c3 = 2;
+const expected3 = true;
+test("test3", c3, expected3);
