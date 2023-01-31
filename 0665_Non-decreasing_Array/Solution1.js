794583930f1ad58343eff610fec14b52f72a3def
Siria <siria55lee@gmail.com>
2023-01-31 11:26:38 +0800

    update

--- 0665_Non-decreasing_Array/Solution1.js
+++ 0665_Non-decreasing_Array/Solution1.js
@@ -2,12 +2,11 @@
  * @param {number[]} nums
  * @return {boolean}
  */
- var checkPossibility = function(nums) {
-  let size = nums.length;
-  if (size <= 1)
+var checkPossibility = function(nums) {
+  if (nums.length === 1)
     return true;
   let moded = nums[0] > nums[1];
-  for (let i = 1; i < size - 1; ++i) {
+  for (let i = 1; i < nums.length - 1; ++i) {
     if (nums[i] <= nums[i+1])
       continue;
     if (moded)
@@ -29,14 +28,18 @@ function test(test_name, nums, expected) {
     console.log(test_name + ' fail');
 }
 
-let nums1 = [4,2,3];
-let expected1 = true;
+const nums1 = [4,2,3];
+const expected1 = true;
 test('test1', nums1, expected1);
 
-let nums2 = [4,2,1];
-let expected2 = false;
+const nums2 = [4,2,1];
+const expected2 = false;
 test('test2', nums2, expected2);
 
-let nums3 = [3,4,2,3];
-let expected3 = false;
+const nums3 = [3,4,2,3];
+const expected3 = false;
 test('test3', nums3, expected3);
+
+const nums4 = [1,4,1,2];
+const expected4 = true;
+test('test4', nums4, expected4);
--- t.js
+++ t.js
@@ -1,30 +1,45 @@
-import ArrayUtils from './util_js/array.js';
-
 /**
- * @param {number[][]} people
- * @return {number[][]}
+ * @param {number[]} nums
+ * @return {boolean}
  */
-var reconstructQueue = function(people) {
-  people.sort((a, b) => {
-    if (a[0] === b[0])
-      return a[1] - b[1];
-    return b[0] - a[0];
-  });
-  let res = [];
-  for (const pair of people) {
-    res.splice(pair[1], 0, pair);
+var checkPossibility = function(nums) {
+  if (nums.length === 1)
+    return true;
+  let moded = nums[0] > nums[1];
+  for (let i = 1; i < nums.length - 1; ++i) {
+    if (nums[i] <= nums[i+1])
+      continue;
+    if (moded)
+      return false;
+    if (nums[i-1] <= nums[i+1])
+      nums[i] = nums[i+1];
+    else
+      nums[i+1] = nums[i];
+    moded = true;
   }
-  return res;
+  return true;
 };
 
-function test(test_name, people, expected) {
-  const res = reconstructQueue(people);
-  if (ArrayUtils.isEqualArray(res, expected))
+function test(test_name, nums, expected) {
+  let res = checkPossibility(nums);
+  if (res === expected)
     console.log(test_name + ' succeed');
   else
     console.log(test_name + ' fail');
 }
 
-const people1 = [[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]];
-const expected1 = [[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]];
-test('test1', people1, expected1)
+const nums1 = [4,2,3];
+const expected1 = true;
+test('test1', nums1, expected1);
+
+const nums2 = [4,2,1];
+const expected2 = false;
+test('test2', nums2, expected2);
+
+const nums3 = [3,4,2,3];
+const expected3 = false;
+test('test3', nums3, expected3);
+
+const nums4 = [1,4,1,2];
+const expected4 = true;
+test('test4', nums4, expected4);
